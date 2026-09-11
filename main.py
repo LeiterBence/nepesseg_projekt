0bb118797fd8613ef23d58f450403b731e9bc3f9
LeiterBence <bence.le1206@gmail.com>
2026-09-11 18:49:02 +0200

    elso resz megcsinalva

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ with open('lakossag_2025.csv', "r" ,encoding='UTF-8') as fajl:
         }
         telepulesek.append(telepules)
 
-def mainmenu():
+def menu_ui():
     print("--------------------------------------------------------------")    
     print()
     print("Népesség 2025".center(60))
@@ -24,16 +24,61 @@ def mainmenu():
     print("--------------------------------------------------------------")    
     print()
     mainmenu_valasztas = input("Válassz a továbblépéshez: ")
+    return mainmenu_valasztas
 
-mainmenu()
-
-def megyeadatok(telepulesek):
+#1. valaszas
+def megyeadatok_ui(telepulesek):
     bekeres_megyekód = input("Kérem a megye kódját: ")
-    print
+
+    megye_telepules_adatok = megyetelepulesek(telepulesek,bekeres_megyekód)
+    print()
+    print(f"Települések száma: {megye_telepules_adatok[0]} db")
+    print(f"Összes lakos: {megye_telepules_adatok[1]} fő")
+    print(f"Városok lakosai összesen: {megye_telepules_adatok[2]} fő")
+    print()
+    print("[Enter] Vissza")
+    print()
+    input()
+
+    
 
 def megyetelepulesek(telepulesek, bekeres_megyekod):
-    
+    telepulesszamok = 0
+    osszlakos = 0
+    varoslakos = 0
     for i in telepulesek:
-        if i["megyekod"] = bekeres_megyekod:
-            
+        if i["megyekod"] == bekeres_megyekod:
+            telepulesszamok += 1
+            osszlakos += i["ferfi_szam"] + i["no_szam"]
+            if i["telepules_tipus"] == "város" or i["telepules_tipus"] == "fővárosi kerület" or i["telepules_tipus"] == "vármegye székhely" or i["telepules_tipus"] == "vármegyei jogú város":
+                varoslakos += i["ferfi_szam"] + i["no_szam"]
+    return [telepulesszamok, osszlakos,varoslakos]
+
+#2. valasztas
+def telepules_tipusok_ui(telepulesek):
+    print("Települések Típusai")
+    print()
+    print("[a] Község")
+    print("[b] Város")
+    print()
+    print("[x] Vissza")
+    valasztas = input("")
+    if valasztas == "a":
+        print("kozseg")
+    elif valasztas == "b":
+        print("varos")
+
+def telepules_tipus_listazas(telepulesek, valasztas):
+    kivalasztott_telepulesek = []
+    sor_szamlalo = 0
+    pass
+
+
+def main():
+    #menu_ui()
+    #megyeadatok_ui(telepulesek)
+    telepules_tipusok_ui(telepulesek)
+
+main()
+
 
